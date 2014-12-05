9c151ba9d0a59cb41ea257756adb40a4cf3d4af3
Matthew Yang <imatthewyang@gmail.com>
2014-12-05 14:00:22 -0800

    Completed numbers exercise and one for the string exercise

--- BlocExercises/BlocExercises/SimpleCalculator.m
+++ BlocExercises/BlocExercises/SimpleCalculator.m
@@ -12,34 +12,21 @@
 
 - (NSInteger) increaseNumberBy1:(NSInteger) number {
     /* WORK HERE */
-    
-    NSInteger newNumber = 1;
-    NSLog(@"newNumber is now %d", newNumber++);
-    
+    number++;
     return number;
 }
 
 - (NSInteger) addNumber:(NSInteger) number1 toNumber:(NSInteger) number2 {
     /* WORK HERE */
-    
-    NSInteger newNumber1 = 5;
-    NSInteger newNumber2 = 6;
-    NSInteger newNumber3 = newNumber1 + newNumber2;
-    
-    NSLog(@"Simple addition...this should equal %d", newNumber3);
-    
+    number1 = number1 + number2;
     return number1;
 }
 
 - (NSInteger) remainderOfNumber:(NSInteger) dividend dividedByNumber:(NSInteger) divisor {
     /* WORK HERE */
     
-    NSInteger newDividend = 42;
-    NSInteger newDivisor = 5;
-    
-    NSLog(@"The remainder of 42 / 5 is %d", newDividend % newDivisor);
-    
-    return 0;
+    NSInteger finalNumber = dividend % divisor;
+    return finalNumber;
 }
 
 @end
--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -12,18 +12,22 @@
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
     /* WORK HERE */
-    
-    return nil;
+    NSString *fullSentence = [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
+    return fullSentence;
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    NSString *shortCheese = cheeseName;
+    shortCheese = [shortCheese stringByReplacingOccurrencesOfString:@"cheese" withString:@"" options:NSCaseInsensitiveSearch range:NSMakeRange(0, [shortCheese length])];
+    
+    return shortCheese;
 }
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
     if (cheeseCount == 1) {
         /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
+        
     } else {
         /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
     }
